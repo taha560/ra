d4963ffaab9ab3318b2cbf5ea368db8768ae6f36
mahdi1062 <mahdikh6@gmail.com>
2016-04-30 07:44:19 -0700

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -208,7 +208,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-	text = "فقط در سوپرگروها فعال است برای خرید گروه به آیدی @mohammadarak مراجعه کنید"
+	text = "با سلام لطفا به ایدی  @pic5all مراجعه کنید برای درخواست گروه"
      	return text
     end
 
