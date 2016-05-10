447b49e09e16aba28e5dbb8e6e5b8943a2e4cf02
mahdi1062 <mahdikh6@gmail.com>
2016-05-10 04:46:11 -0700

    Update Group_Users.lua

--- plugins/Group_Users.lua
+++ plugins/Group_Users.lua
@@ -8,7 +8,7 @@ local function set_pass(msg, pass, id)
   local name = string.gsub(msg.to.print_name, '_', '')
   if hash then
     redis:hset(hash, pass, id)
-      return send_large_msg("chat#id"..msg.to.id, "🔱 User of Group:\n["..name.."] has been set to:‌\n\n "..pass.."\n\nNow user can join in pm by\njoin "..pass.." ⚜", ok_cb, true)
+      return send_large_msg("chat#id"..msg.to.id, "ðŸ”± User of Group:\n["..name.."] has been set to:â€Œ\n\n "..pass.."\n\nNow user can join in pm by\njoin "..pass.." âšœ", ok_cb, true)
   end
 end
 
@@ -18,9 +18,9 @@ local function is_used(pass)
   return used or false
 end
 local function show_add(cb_extra, success, result)
-  —vardump(result)
+  --vardump(result)
     local receiver = cb_extra.receiver
-    local text = "I added you to👥 "..result.title.."(👤"..result.participants_count..")"
+    local text = "I added you toðŸ‘¥ "..result.title.."(ðŸ‘¤"..result.participants_count..")"
     send_large_msg(receiver, text)
 end
 local function added(msg, target)
@@ -28,11 +28,11 @@ local function added(msg, target)
   chat_info("chat#id"..target, show_add, {receiver=receiver})
 end
 local function run(msg, matches)
-  if matches[1] == "/user" and msg.to.type == "chat" and matches[2] then
+  if matches[1] == "user" and msg.to.type == "chat" and matches[2] then
     local pass = matches[2]
     local id = msg.to.id
     if is_used(pass) then
-      return "این یوزر نیم قابل استفاده نیست"
+      return "Ø§ÛŒÙ† ÛŒÙˆØ²Ø± Ù†ÛŒÙ… Ù‚Ø§Ø¨Ù„ Ø§Ø³ØªÙØ§Ø¯Ù‡ Ù†ÛŒØ³Øª"
     end
     redis:del("setpass:", id)
     return set_pass(msg, pass, id)
@@ -43,14 +43,14 @@ local function run(msg, matches)
     local id = redis:hget(hash, pass)
     local receiver = get_receiver(msg)
     if not id then
-      return "گروهی با این یوزر نیم وجود ندارد"
+      return "Ú¯Ø±ÙˆÙ‡ÛŒ Ø¨Ø§ Ø§ÛŒÙ† ÛŒÙˆØ²Ø± Ù†ÛŒÙ… ÙˆØ¬ÙˆØ¯ Ù†Ø¯Ø§Ø±Ø¯"
     end
     chat_add_user("chat#id"..id, "user#id"..msg.from.id, ok_cb, false) 
   return added(msg, id)
   else
-  return " من نمیتوانم شما را به"..string.gsub(msg.to.id.print_name, '_', ' ').."اضافه کنم"
+  return " Ù…Ù† Ù†Ù…ÛŒØªÙˆØ§Ù†Ù… Ø´Ù…Ø§ Ø±Ø§ Ø¨Ù‡"..string.gsub(msg.to.id.print_name, '_', ' ').."Ø§Ø¶Ø§ÙÙ‡ Ú©Ù†Ù…"
   end
-  if matches[1] == "/user" then
+  if matches[1] == "user" then
    local hash = 'setpass:'
    local chat_id = msg.to.id
    local pass = redis:hget(hash, chat_id)
